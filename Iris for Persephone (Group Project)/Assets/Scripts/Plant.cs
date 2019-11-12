95e5b64706479a40c8ccdae63d15d1e2c81b0aa9
Dane Claessen <danec99@gmail.com>
2019-11-12 15:51:19 -0600

    adds plants

---
+++ Iris for Persephone (Group Project)/Assets/Scripts/Plant.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plant : MonoBehaviour
+{
+
+    Renderer plant_Renderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        plant_Renderer = GetComponent<Renderer>();
+        plant_Renderer.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            plant_Renderer.enabled = true;
+        }
+
+    }
+}
