3a0387035e91974aa95f9a281ad46a3dc518a7cf
bheffing <42662595+bheffing@users.noreply.github.com>
2019-11-17 12:28:52 -0600

    added the transition scene; need to add day tracking

---
+++ Assets/Scripts/LevelChanger.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelChanger : MonoBehaviour
+{
+    public Animator animator;
+    private int levelToLoad;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            FadeToLevel(1);
+        }
+    }
+
+    public void FadeToLevel (int levelIndex)
+    {
+        levelToLoad = levelIndex;
+        animator.SetTrigger("FadeOut");
+    }
+
+    public void OnFadeComplete ()
+    {
+        SceneManager.LoadScene(levelToLoad);
+    }
+}
