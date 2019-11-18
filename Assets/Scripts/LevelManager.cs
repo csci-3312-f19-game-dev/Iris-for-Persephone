0f3ba5e2d2493e7cbd956e43dae552646a22a86d
Dane Claessen <danec99@gmail.com>
2019-11-18 00:49:06 -0600

    Adds map generation and map assets

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : Singleton<LevelManager>
+{
+    [SerializeField]
+    private GameObject[] tilePrefabs;
+
+    [SerializeField]
+    private CameraMovement cameraMovement;
+
+    private Point vortexSpawnLoc, phantomSpawnLoc;
+    [SerializeField]
+    private GameObject vortexSpawnPrefab;
+    [SerializeField]
+    private GameObject phantomSpawnPrefab;
+
+
+    public Dictionary<Point, TileScript> Tiles { get; set; }
+
+    public float TileSize
+    {
+        get { return tilePrefabs[0].GetComponent<SpriteRenderer>().sprite.bounds.size.x; }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CreateLevel();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private void CreateLevel()
+    {
+        Tiles = new Dictionary<Point, TileScript>();
+
+        Vector3 topLeft = Camera.main.ScreenToWorldPoint(new Vector3(0, Screen.height));
+        Vector3 botRight = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, 0));
+        int gridWidth = (int) ((botRight.x - topLeft.x) / TileSize) + 1;
+        int gridHeight = (int)((topLeft.y - botRight.y) / TileSize) + 1;
+        for (int x = 0; x < gridWidth; x++)
+        {
+            for(int y = 0; y < gridHeight; y++)
+            {
+                PlaceTile(x, y, topLeft);
+            }
+        }
+
+        Vector3 maxTile = Tiles[new Point(gridWidth - 1, gridHeight - 1)].transform.position;
+
+        cameraMovement.SetLimits(new Vector3(maxTile.x + TileSize/2, maxTile.y - TileSize/2));
+        PlaceSpawner();
+    }
+
+    private void PlaceTile(int x, int y, Vector3 topLeft)
+    {
+        TileScript newTile = Instantiate(tilePrefabs[0]).GetComponent<TileScript>();
+        newTile.GetComponent<TileScript>().Setup(new Point(x, y), 
+                                                 new Vector3(topLeft.x + TileSize * x + TileSize / 2, topLeft.y - TileSize * y - TileSize / 2, 0.0f));
+    }
+
+    private void PlaceSpawner()
+    {
+        vortexSpawnLoc = new Point(0, 0);
+        Instantiate(vortexSpawnPrefab, Tiles[vortexSpawnLoc].GetComponent<TileScript>().WorldPos, Quaternion.identity);
+    }
+}
---
+++ Assets/Scripts/TileScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileScript : MonoBehaviour
+{
+    public Point GridPosition { get; private set; }
+
+    public Vector2 WorldPos
+    {
+        get
+        {
+            return new Vector2(transform.position.x - GetComponent<SpriteRenderer>().bounds.size.x / 2, transform.position.y + GetComponent<SpriteRenderer>().bounds.size.y / 2);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Setup(Point gridPos, Vector3 worldPos)
+    {
+        transform.position = worldPos;
+        this.GridPosition = gridPos;
+
+        LevelManager.Instance.Tiles.Add(gridPos, this);
+    }
+}
