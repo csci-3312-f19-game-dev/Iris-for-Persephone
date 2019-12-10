46a57d68f10a9a284bd2fae3ec7a66a01aec814c
bheffing <42662595+bheffing@users.noreply.github.com>
2019-12-10 14:57:43 -0600

    look at all those chickens

---
+++ Assets/Scripts/SoundHandler.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundHandler : Singleton<SoundHandler>
+{
+    public AudioSource hitSounds, music;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void HitSound() { hitSounds.Play(); }
+}
